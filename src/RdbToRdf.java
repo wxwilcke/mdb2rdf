fdb76ba6ef9525b84a701d016ede14c6ad89628c
Xander Wilcke <w.x.wilcke@vu.nl>
2015-05-10 15:30:33 +0200

    Partial Support for Literals added

--- src/RdbToRdf.java
+++ src/RdbToRdf.java
@@ -10,6 +10,7 @@ import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.ResourceFactory;
 import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.Statement;
 import com.hp.hpl.jena.rdf.model.ResIterator;
 
@@ -71,7 +72,7 @@ public class RdbToRdf {
 
 
 					// create and add all attributes of resource
-					List<Statement> sa	= convertRowToStatement(row, rcs);
+					List<Statement> sa	= convertRowToStatement(table, row, rcs);
 					addRowToMODEL(sa, key, tableName);
 				}
 			}
@@ -136,18 +137,19 @@ public class RdbToRdf {
 
 	// create an array of statements from the attribute-value pairs within the
 	// row
-	private List<Statement> convertRowToStatement(Row row, Resource rcs) {
+	private List<Statement> convertRowToStatement(Table table, Row row, Resource rcs) {
 		List<Statement> sa	= new ArrayList<Statement>(row.size()); 
 		int i = 0;
 		
 		Set<String> attrs = row.keySet();
 		for (String attr : attrs) {
-			Resource attrRcs;
+			RDFNode attrRcs;
 			Object value	= row.get(attr);
 			if (value == null) { // dealing with empty values
 				attrRcs	= ResourceFactory.createResource(); //bnode
 			} else {
-				attrRcs	= ResourceFactory.createResource(genRURI(value.toString()));
+				attrRcs	= createRDFNode(table.getColumn(attr), value);
+			//	attrRcs	= ResourceFactory.createResource(genRURI(value.toString()));
 			}
 
 			Property p	= ResourceFactory.createProperty(genOURI(), attr.toString());
@@ -157,6 +159,56 @@ public class RdbToRdf {
 		}
 
 		return sa;
+	}
+
+	private RDFNode createRDFNode(Column column, Object value) {
+		RDFNode node	= ResourceFactory.createResource(); //bnode
+		
+		DataType t	= column.getType();
+		switch(t) {
+			case TEXT:	
+			case MEMO:
+				if (isLiteralValue(value.toString())) {
+					// TODO: add language option
+					node	= ResourceFactory.createPlainLiteral(value.toString());
+					break;
+				}
+
+				node	= ResourceFactory.createResource(resolveURI(value.toString()));
+				break;
+
+			default:
+				// TODO: add datatype
+				node	= ResourceFactory.createPlainLiteral(value.toString());
+				break;
+		}
+
+		return node;
+	}
+
+	// heuristical method to determine whether it is a literal or not
+	private Boolean isLiteralValue(String s) {
+		return (isSentence(s) || isURL(s));
+	}
+
+	private Boolean isSentence(String s) {
+		//return s.matches("[^.!?\\s][^.!?]*(?:[.!?](?!['\"]?\\s|$)[^.!?]*)*[.!?]?['\"]?(?=\\s|$)");
+		return s.matches("[^.!?\\s][^.!?]*(?:[.!?](?!['\"]?$)[^.!?]*)*[.!?]?['\"]?(?=\\s|$)");
+	}
+
+	private Boolean isURL(String s) {
+		// source: mathiasbynens.be/demo/url-regex
+		return s.matches("_^(?:(?:https?|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?!10(?:\\.\\d{1,3}){3})(?!127(?:\\.\\d{1,3}){3})(?!169\\.254(?:\\.\\d{1,3}){2})(?!192\\.168(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\x{00a1}-\\x{ffff}0-9]+-?)*[a-z\\x{00a1}-\\x{ffff}0-9]+)(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}0-9]+-?)*[a-z\\x{00a1}-\\x{ffff}0-9]+)*(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}]{2,})))(?::\\d{2,5})?(?:/[^\\s]*)?$_iuS");
+	}
+
+	private String resolveURI(String value) {
+		String uri	= new String();
+
+		// TODO: search Semantic Web for existing one
+
+		uri	= genRURI(value);
+
+		return uri;
 	}
 
 	//generate master resource uri
